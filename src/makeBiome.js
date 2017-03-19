68b385f738645f71e2f9e4390ba480f319ea23d1
Cevher <cevherkarakoc@live.com>
2017-03-19 11:10:15 +0200

    hight to height

--- src/makeBiome.js
+++ src/makeBiome.js
@@ -2,34 +2,34 @@ import {Ocean, Water, Beach,
         Snow, Grass, Stone, Desert,
         TropicalForest, TaigaForest } from "./biomes";
 
-const makeBiome = (hight, moisture) => {
+const makeBiome = (height, moisture) => {
   var zone;
 
-  if ( hight > 0.55 ) {
-    if ( moisture <0.5 ) zone = new Stone(hight);
-    else                 zone = new Snow(hight) ;
+  if ( height > 0.55 ) {
+    if ( moisture <0.5 ) zone = new Stone(height);
+    else                 zone = new Snow(height) ;
   }
 
-  else if ( hight> 0.40 ) {
-    if ( moisture <= 0.25 )     zone = new Desert(hight)     ;
-    else if ( moisture < 0.50 ) zone = new Stone(hight)      ;
-    else                        zone = new TaigaForest(hight);
+  else if ( height> 0.40 ) {
+    if ( moisture <= 0.25 )     zone = new Desert(height)     ;
+    else if ( moisture < 0.50 ) zone = new Stone(height)      ;
+    else                        zone = new TaigaForest(height);
   }
 
-  else if ( hight > 0.20 ) {
-    if ( moisture <= 0.25 )   zone = new Desert(hight)        ;
-    else if (moisture < 0.50) zone = new Grass(hight)         ;
-    else                      zone = new TropicalForest(hight);
+  else if ( height > 0.20 ) {
+    if ( moisture <= 0.25 )   zone = new Desert(height)        ;
+    else if (moisture < 0.50) zone = new Grass(height)         ;
+    else                      zone = new TropicalForest(height);
   }
 
-  else if ( hight> 0.15 ) {
-    if ( moisture <= 0.25 )     zone = new Desert(hight)        ;
-    else if ( moisture < 0.75 ) zone = new Grass(hight)         ;
-    else                        zone = new TropicalForest(hight);
+  else if ( height> 0.15 ) {
+    if ( moisture <= 0.25 )     zone = new Desert(height)        ;
+    else if ( moisture < 0.75 ) zone = new Grass(height)         ;
+    else                        zone = new TropicalForest(height);
   }
 
-  else if ( hight> 0.12 ) zone = new Beach(hight);
-  else if ( hight> 0.08 ) zone = new Water();
+  else if ( height> 0.12 ) zone = new Beach(height);
+  else if ( height> 0.08 ) zone = new Water();
   else zone = new Ocean;
 
   return zone; 
